5de5da59ce303c5ac9bf2c1aef5c965d5fe3d4bf
DURBAroy111 <120655973+DURBAroy111@users.noreply.github.com>
2023-12-09 22:26:15 +0600

    Update main.cpp

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include "matrixoperation.h"
+
+
+int main() {
+   int rows = 3;
+    int cols = 3;
+
+    std::cout<<"Enter your first matrix element\n";
+    std::vector<std::vector<int>> matrix1 = createMatrixWithInput(rows, cols);
+    std::cout<<"Enter your second matrix element\n";
+    std::vector<std::vector<int>> matrix2 = createMatrixWithInput(rows, cols);
+
+
+    std::vector<std::vector<int>> result = addMatrices(matrix1, matrix2);
+    std::vector<std::vector<int>> subresult = subtractMatrices(matrix1, matrix2);
+    std::vector<std::vector<int>> multiplyresult = multiplyMatrices(matrix1, matrix2);
+
+    std::cout<<"Matrix Addition Result\n";
+    displayMatrix(result);
+
+    std::cout<<"Matrix Subtraction Result\n";
+    displayMatrix(subresult);
+
+    std::cout<<"Matrix multiplication result\n";
+    displayMatrix(multiplyresult);
+
+
+
+   std::vector<std::vector<double>> matrix1 = createRowReductionMatrix(3);
+
+    // Find the solution
+    std::vector<double> solution = rowReductionSolve(matrix1);
+
+    // Display the solution
+    displayRowReductionSolve(solution);
+    
+
+
+   std::vector<std::vector<double>> matrix1 = createMatrixForInverseOrDet(3);
+
+    // Find the inverse matrix
+   // std::vector<std::vector<double>> inverseMatrix = findMatrixInverse(matrix1);
+
+
+  // std::vector<std::vector<double>> matrix = createMatrixForInverseOrDet(3);
+
+    // Find the determinant
+    std::cout<< findMatrixDeterminant(matrix1);
+
+
+    return 0;
+}
